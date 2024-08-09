f49d8125c7c3372f08966a46203c657327f5e1c0
glen <glen84_dr@hotmail.com>
2024-08-09 13:19:16 +0200

    refacto and adding utf-8

--- test_serv.py
+++ test_serv.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
-hostName = "localhost"
+hostName = "0.0.0.0"
 serverPort = 9998
 
 # Launch server commande: python -m http.server
@@ -9,41 +9,32 @@ serverPort = 9998
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path == "glen":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        
+        if self.path == "/glen":
             self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-            self.wfile.write(bytes("<h3> This is the first page of basic python web server</h3>"))
-            self.wfile.write(bytes("<p> Lorem ipsus 1 and termus primus</p>"))
-            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>"))
+            self.wfile.write(bytes("<h3> This is the first page of basic python web server</h3>", "utf-8"))
+            self.wfile.write(bytes("<p> Lorem ipsus 1 and termus primus</p>", "utf-8"))
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8", "utf-8"))
+
+        elif self.path == "/filao":
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<h3> This is the Second page of basic python web server</h3>", "utf-8"))
+            self.wfile.write(bytes("<p> Lorem ipsus 2 arrivas secund y nihli!</p>", "utf-8"))
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>", "utf-8"))
             self.wfile.write(bytes("</body></html>", "utf-8"))
 
-        elif self.path == "filao":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
+        elif self.path == "/lastPage":
             self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-            self.wfile.write(bytes("<h3> This is the Second page of basic python web server</h3>"))
-            self.wfile.write(bytes("<p> Lorem ipsus 2 arrivas secund y nihli!</p>"))
-            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>"))
-            self.wfile.write(bytes("</body></html>", "utf-8"))
-
-        elif self.path == "lastPage":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-            self.wfile.write(bytes("<h3> This is the Third page of basic python web server</h3>"))
-            self.wfile.write(bytes("<p> Lorem ipsus 3 triptic in greec</p>"))
-            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>"))
-            self.wfile.write(bytes("</body></html>", "utf-8"))
+            self.wfile.write(bytes("<h3> This is the Third page of basic python web server</h3>", "utf-8"))
+            self.wfile.write(bytes("<p> Lorem ipsus 3 triptic in greec</p>", "utf-8"))
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8", "utf-8"))
 
 
 if __name__ == "__main__":
