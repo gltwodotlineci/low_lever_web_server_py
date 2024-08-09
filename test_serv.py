4cbea1e5571c97e55ac1a6f56d6133d832c5f9f1
glen <glen84_dr@hotmail.com>
2024-08-09 11:39:09 +0200

    beginign of project

---
+++ test_serv.py
@@ -0,0 +1,58 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 9998
+
+# Launch server commande: python -m http.server
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "glen":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<h3> This is the first page of basic python web server</h3>"))
+            self.wfile.write(bytes("<p> Lorem ipsus 1 and termus primus</p>"))
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+
+        elif self.path == "filao":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<h3> This is the Second page of basic python web server</h3>"))
+            self.wfile.write(bytes("<p> Lorem ipsus 2 arrivas secund y nihli!</p>"))
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+
+        elif self.path == "lastPage":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<h3> This is the Third page of basic python web server</h3>"))
+            self.wfile.write(bytes("<p> Lorem ipsus 3 triptic in greec</p>"))
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName,serverPort),MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped")
