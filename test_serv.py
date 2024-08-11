82c2fd4ecbc6fa8ca86337097c76bb53c42421b8
glen <glen84_dr@hotmail.com>
2024-08-11 11:58:41 +0200

    limiting server response to inexistents paths

--- test_serv.py
+++ test_serv.py
@@ -16,31 +16,34 @@ def return_pages(given_self):
     elif given_self.path == '/lastPage':
         return given_self.wfile.write(bytes("<h3>This is the Third page of basic python web server</h3>", "utf-8")), given_self.wfile.write(bytes("<p> Lorem ipsus 3 triptic in greec</p>", "utf-8"))
 
-   
     
 class MyServer(BaseHTTPRequestHandler):
-    # Sending the GET requests responses	
+
+    # Sending the GET requests responses
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))        
-        # calling a method for specific 3 pages
-        return_pages(self)
-           
-        self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))            
+
+        if self.path in ['/glen', '/filao','/lastPage']:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            # calling a method for specific 3 pages
+            return_pages(self)
+
+            self.wfile.write(bytes("<p>The link of this turorial is <a href='https://pythonbasics.org/webserver/'>This</a></p>", "utf-8"))
+            self.wfile.write(bytes("<html><head><title>Basic web server</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
 
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName,serverPort),MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    print(" Server started http://%s:%s" % (hostName, serverPort))
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
 
     webServer.server_close()
-    print("Server stopped")
+    print(" Server stopped")
